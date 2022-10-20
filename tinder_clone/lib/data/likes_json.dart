cadbfee28a4530b2e075d450c02d6a893894837d
Lucas Dias <lucasdias@fai-mg.br>
2022-10-20 15:20:40 -0300

    fix: alterado nome do projeto e corrigido referencias de imagens

--- tinder_clone/lib/data/likes_json.dart
+++
@@ -1,5 +0,0 @@
-const List likes_json = [
-  {"img": "/images/campaings/campanha_deupet.jpg", "active": true},
-  {"img": "/images/campaings/campanha_vacina.jpg", "active": true},
-  {"img": "/images/campaings/campanha_castracao.jpg", "active": true},
-];
